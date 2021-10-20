ad39fdfa71ba1393c9fdcccaffcdea585c8624ae
A01067398 <78871665+A01067398@users.noreply.github.com>
2021-10-20 10:55:01 -0500

    Update registroZombie.php

--- PHP/registroZombie.php
+++ PHP/registroZombie.php
@@ -1,6 +1,37 @@
 <?php
 
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-	
+		$nombre = $_POST['nombreZombie'];
+		$estado = $_POST['estadoZombie'];
+
+		$query = "call registrarZombie('$nombre','$estado'); ";
+		$resultado = mysqli_query($conexion,$query);
+
+		if($resultado){
+
+			http_response_code(200);
+			$response['state']=true;
+        	$response['message']="Registro realizado";
+
+		}else{
+
+			http_response_code(400);
+			$response['state']=false;
+        	$response['message']="Error al procesar la consulta";
+
+		}
+
+
+	}else{
+
+		http_response_code(405);
+		$response['state']=false;
+        $response['message']="Metodo HTTP no permitido";
+
+	}
+
+	header('Content-Type: application/json; charset=UTF-8');
+    echo json_encode($response);	
 
 ?>
